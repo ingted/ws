7df0393b4a0a067c70fdb94c5749f1289869c5ef
András Jankó <andras.janko@gmail.com>
2017-03-03 12:27:59 -0800

    remove ForceFoundVersion from Owin.WebSocket dependency

--- build-zafir.fsx
+++ build-zafir.fsx
@@ -22,7 +22,7 @@ let main =
             [
                 r.NuGet("Owin").ForceFoundVersion().Reference()
                 r.NuGet("Microsoft.Owin").ForceFoundVersion().Reference()
-                r.NuGet("Owin.WebSocket").Version("1.6.0").ForceFoundVersion().Reference()
+                r.NuGet("Owin.WebSocket").Version("1.6.0").Reference()
                 r.NuGet("CommonServiceLocator").ForceFoundVersion().Reference()
                 r.NuGet("Zafir.Owin").Latest(true).ForceFoundVersion().Reference()
                 r.File(MPServiceLocation)
