69a563b8dcf5e9dbe0c31aa634454cf116fb8976
András Jankó <andras.janko@gmail.com>
2018-01-29 14:21:57 +0100

    Fixing build to use any prerelease WS deps

--- build.sh
+++ build.sh
@@ -4,9 +4,13 @@ set -e
 
 if [ "$OS" = "Windows_NT" ]; then
     .paket/paket.exe restore -g build
+    .paket/paket.exe update -g wsbuild
+    .paket/paket.exe restore -g build
 else
+    mono .paket/paket.exe restore -g build
+    mono .paket/paket.exe update -g wsbuild
     mono .paket/paket.exe restore -g build
 fi
 
 export NOT_DOTNET=true
-paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.sh "$@"
+paket-files/wsbuild/intellifactory/websharper/tools/WebSharper.Fake.sh "$@"
